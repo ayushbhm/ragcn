91e8e5bd6179d33da5773ad20631f1048bf41e95
ayushbhm <writetoayushv@gmail.com>
2026-07-07 13:21:38 +0530

    bind to 0.0.0.0, use PORT env var, disable debug

--- back/app.py
+++ back/app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from routes.main import main
 
 from flask_cors import CORS
 from dotenv import load_dotenv
 load_dotenv()
-    
+
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -15,6 +16,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 app.config['SESSIONS'] = {}
-app.debug = True
-if __name__ == '__main__': 
- app.run(port=5000)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
